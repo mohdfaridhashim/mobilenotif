dd60aa02f4ba2c85879807351705c9a9ac88e18d
mohd farid <zackseen@gmail.com>
2017-10-20 02:14:47 +0800

    Added browser

--- platforms/browser/platform_www/cordova_plugins.js
+++ platforms/browser/platform_www/cordova_plugins.js
@@ -3,7 +3,8 @@ module.exports = [];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-whitelist": "1.3.1"
+    "cordova-plugin-whitelist": "1.3.1",
+    "cordova-plugin-privacyscreen": "0.3.1"
 }
 // BOTTOM OF METADATA
 });
